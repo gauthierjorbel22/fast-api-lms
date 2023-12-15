6eb88327bbeaa16747cbbe317ab9cede3d2a18a6
Gauthier Kanku <jorbelgauthier@gmail.com>
2023-12-15 19:59:50 +0200

    creating routers for apis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Optional, List
 from fastapi import FastAPI, Path
 from pydantic import BaseModel 
+from api import users, courses, sections
+
+
 
 app = FastAPI(
     title='Fast API LMS', 
@@ -12,23 +15,6 @@ app = FastAPI(
     }
 )
 
-users = []
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-    
-
-@app.get("/users", response_model=List[User])
-async def get_users():
-    return users
-
-@app.post('/users')
-async def create_user(user:User):
-    users.append(user)
-    return {"message": "User created successfully"}
-
-@app.get('/users/{id}')
-async def get_user(id:int=Path(..., description="The ID of the user to get")):
-    return users[id]
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
